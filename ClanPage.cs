5e6593d8e93f090b1dabeedf5b449751989d4871
Lesser Scholar <lesserscholar@gmail.com>
2022-02-09 00:57:49 +0200

    Split pages to files

---
+++ ClanPage.cs
@@ -0,0 +1,46 @@
+﻿using Bannerlord.UIExtenderEx.Attributes;
+using Bannerlord.UIExtenderEx.ViewModels;
+using HarmonyLib;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia;
+using TaleWorlds.Core.ViewModelCollection;
+using TaleWorlds.Localization;
+
+namespace EncyclopediaExtender
+{
+
+    [ViewModelMixin("RefreshValues", true)]
+    public class ExtendEncyclopediaClanPageVM : BaseViewModelMixin<EncyclopediaClanPageVM>
+    {
+        public ExtendEncyclopediaClanPageVM(EncyclopediaClanPageVM vm) : base(vm)
+        {
+
+        }
+        public override void OnRefresh()
+        {
+            var vm = base.ViewModel;
+            if (vm != null)
+            {
+                var clan = Traverse.Create(vm).Field("_clan").GetValue<Clan>();
+                if (vm.ClanInfo.Count <= 3)
+                {
+                    vm.ClanInfo.Add(new StringPairItemVM("", ""));
+                    vm.ClanInfo.Add(new StringPairItemVM(new TextObject("{=beBL5H1u2fu}Cash:").ToString(), clan.Leader.Gold.ToString("N0")));
+                    vm.ClanInfo.Add(new StringPairItemVM(new TextObject("{=C1SUFxYrMXk}Debt:").ToString(), clan.DebtToKingdom.ToString()));
+                    var kingdom = clan.Kingdom;
+                    int kingdom_wealth = 0;
+                    if (kingdom != null && !clan.IsMinorFaction)
+                    {
+                        kingdom_wealth = kingdom.KingdomBudgetWallet / (kingdom.Clans.Count + 1) / 2;
+                        vm.ClanInfo.Add(new StringPairItemVM(new TextObject("{=a2uZeyyIdQX}Share of Kingdom Wealth:").ToString(),
+                            kingdom_wealth.ToString("N0")));
+                    }
+                    vm.ClanInfo.Add(new StringPairItemVM(new TextObject("{=nAr2HzgGiVn}Nominal Total Wealth:").ToString(),
+                        (clan.Leader.Gold + kingdom_wealth - clan.DebtToKingdom).ToString("N0")));
+                }
+
+            }
+        }
+    }
+}
---
+++ FactionPage.cs
@@ -0,0 +1,56 @@
+﻿using Bannerlord.UIExtenderEx.Attributes;
+using Bannerlord.UIExtenderEx.Prefabs2;
+using Bannerlord.UIExtenderEx.ViewModels;
+using HarmonyLib;
+using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia;
+using TaleWorlds.Core.ViewModelCollection;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+
+namespace EncyclopediaExtender
+{
+    [PrefabExtension("EncyclopediaFactionPage", "descendant::EncyclopediaSubPageElement[@Id='Leader']")]
+    public class FactionPagePatch : PrefabExtensionInsertPatch
+    {
+        public override InsertType Type => InsertType.Append;
+
+        [PrefabExtensionFileName(true)]
+        public String File => "FactionPagePatch";
+    }
+
+    [ViewModelMixin("RefreshValues", true)]
+    public class ExtendEncyclopediaFactionPageVM : BaseViewModelMixin<EncyclopediaFactionPageVM>
+    {
+        [DataSourceProperty]
+        public MBBindingList<StringPairItemVM> WealthInfo { get; set; }
+        public ExtendEncyclopediaFactionPageVM(EncyclopediaFactionPageVM vm) : base(vm)
+        {
+            WealthInfo = new MBBindingList<StringPairItemVM>();
+            KingdomWealthText = "";
+        }
+        [DataSourceProperty]
+        public String KingdomWealthText { get; set; }
+        public override void OnRefresh()
+        {
+            KingdomWealthText = new TextObject("{=VidmvRvXecq}Kingdom Wealth").ToString();
+            WealthInfo.Clear();
+            var vm = base.ViewModel;
+            if (vm != null)
+            {
+                var kingdom = Traverse.Create(vm).Field("_faction").GetValue<Kingdom>();
+
+                var kingdomBankText = new TextObject("");
+                WealthInfo.Add(new StringPairItemVM(new TextObject("{=z4z97KSX12m}Kingdom Bank:").ToString(), kingdom.KingdomBudgetWallet.ToString("N0")));
+                int clans_wealth = 0;
+                foreach (var clan in kingdom.Clans)
+                {
+                    if (clan.IsUnderMercenaryService || clan.IsMinorFaction) continue;
+                    clans_wealth += clan.Leader.Gold - clan.DebtToKingdom;
+                }
+                WealthInfo.Add(new StringPairItemVM(new TextObject("{=Ks1B7PO9DjP}Sum of Clan Wealth:").ToString(), clans_wealth.ToString("N0")));
+            }
+        }
+    }
+}
