9f1337dc1d517cd528702c038fffe86caaf865d0
adwitkow <adamadik@poczta.fm>
2025-09-17 01:09:24 +0200

    Ensure backwards compatibility for versions prior to 1.3.0

--- Bannerlord.EncyclopediaExtender/EncyclopediaSettlementPage/ViewModels/ItemElementVM.cs
+++ Bannerlord.EncyclopediaExtender/EncyclopediaSettlementPage/ViewModels/ItemElementVM.cs
@@ -1,6 +1,8 @@
 ﻿using TaleWorlds.Core;
 using TaleWorlds.Core.ViewModelCollection;
+#if v130
 using TaleWorlds.Core.ViewModelCollection.ImageIdentifiers;
+#endif
 
 namespace Bannerlord.EncyclopediaExtender.EncyclopediaSettlementPage.ViewModels
 {
@@ -8,7 +10,11 @@ namespace Bannerlord.EncyclopediaExtender.EncyclopediaSettlementPage.ViewModels
     {
         public ItemElementVM(ItemObject item)
         {
+#if v130
             base.ImageIdentifier = new ItemImageIdentifierVM(item);
+#else
+            base.ImageIdentifier = new ImageIdentifierVM(item);
+#endif
             base.ItemDescription = item.Name.ToString();
         }
     }
